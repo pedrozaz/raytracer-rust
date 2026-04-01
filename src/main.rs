4236ad718fd32c08c230be1234b14ef166f803f7
Guilherme Pedroza <guilhermebarb0sa@proton.me>
2026-04-01 20:34:42 -0300

    feat: ppm and output image format

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let width: i16 = 200;
+    let height: i16 = 100;
+
+    println!("P3\n{} {}\n255", width, height);
+
+    for j in (0..height).rev() {
+        for i in 0..width {
+            let r = i as f64 / width as f64;
+            let g = j as f64 / height as f64;
+            let b = 0.2;
+
+            let ir = (255.99 * r) as i32;
+            let ig = (255.99 * g) as i32;
+            let ib = (255.99 * b) as i32;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
 }
