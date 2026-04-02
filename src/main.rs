ba84bed8cfa308b6a1894c64bf340af364cb1cc2
Guilherme Pedroza <guilhermebarb0sa@proton.me>
2026-04-02 11:44:28 -0300

    feat: diffuse materials, recursive ray bounding and gamma correction

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ mod ray;
 mod sphere;
 mod vec3;
 
+use core::f64;
+
 use camera::Camera;
 use hittable::Hittable;
 use hittable_list::HittableList;
-use rand::RngExt;
+use rand::{RngExt, random};
 use ray::Ray;
 use sphere::Sphere;
 use vec3::Vec3;
@@ -23,22 +25,38 @@ fn hit_sphere(center: Vec3, radius: f64, ray: &Ray) -> bool {
     discriminant > 0.0
 }
 
-fn calculate_color(r: &Ray, world: &dyn Hittable) -> Vec3 {
-    if let Some(record) = world.hit(r, 0.0, f64::INFINITY) {
-        return Vec3::new(
-            record.normal.x + 1.0,
-            record.normal.y + 1.0,
-            record.normal.z + 1.0,
-        ) * 0.5;
+fn calculate_color(r: &Ray, world: &dyn Hittable, rng: &mut impl rand::Rng, depth: i32) -> Vec3 {
+    if depth <= 0 {
+        return Vec3::new(0.0, 0.0, 0.0);
+    }
+
+    if let Some(record) = world.hit(r, 0.001, f64::INFINITY) {
+        let target = record.p + record.normal + random_in_unit_sphere(rng);
+        let scattered = Ray::new(record.p, target - record.p);
+
+        return calculate_color(&scattered, world, rng, depth - 1) * 0.5;
     }
 
     let unit_direction = r.direction.normalize();
     let t = 0.5 * (unit_direction.y + 1.0);
-
     let white = Vec3::new(1.0, 1.0, 1.0);
     let blue = Vec3::new(0.5, 0.7, 1.0);
 
     (white * (1.0 - t)) + (blue * t)
+}
+
+fn random_in_unit_sphere(rng: &mut impl rand::Rng) -> Vec3 {
+    loop {
+        // rng.random::<f64> returns [0.0, 1.0). Mapped to [-1.0, 1.0)
+        let p = Vec3::new(
+            rng.random::<f64>() * 2.0 - 1.0,
+            rng.random::<f64>() * 2.0 - 1.0,
+            rng.random::<f64>() * 2.0 - 1.0,
+        );
+        if p.squared_lenght() < 1.0 {
+            return p;
+        }
+    }
 }
 
 fn main() {
@@ -71,10 +89,10 @@ fn main() {
                 let v = (j as f64 + rng.random::<f64>()) / ny as f64;
 
                 let r = camera.get_ray(u, v);
-                color = color + calculate_color(&r, &world);
+                color = color + calculate_color(&r, &world, &mut rng, 50);
             }
 
-            color = color / samples_per_pixel as f64;
+            color = Vec3::new(color.x.sqrt(), color.y.sqrt(), color.z.sqrt());
 
             let ir = (255.99 * color.x) as i32;
             let ig = (255.99 * color.y) as i32;
