d63992480f8c1ead6087e2df4fe2ddfdfd9efcd0
Guilherme Pedroza <guilhermebarb0sa@proton.me>
2026-04-01 22:18:25 -0300

    feat: implement Ray function and background linear blend

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,43 @@
 mod ray;
 mod vec3;
 
+use ray::Ray;
+use vec3::Vec3;
+
+fn color(r: &Ray) -> Vec3 {
+    let unit_direction = r.direction.normalize();
+
+    let t = 0.5 * (unit_direction.y + 1.0);
+    let white = Vec3::new(1.0, 1.0, 1.0);
+    let blue = Vec3::new(0.5, 0.7, 1.0);
+
+    (white * (1.0 - t)) + (blue * t)
+}
+
 fn main() {
-    let width: i16 = 200;
-    let height: i16 = 100;
+    let nx = 200;
+    let ny = 100;
 
-    println!("P3\n{} {}\n255", width, height);
+    println!("P3\n{} {}\n255", nx, ny);
 
-    for j in (0..height).rev() {
-        for i in 0..width {
-            let r = i as f64 / width as f64;
-            let g = j as f64 / height as f64;
-            let b = 0.2;
+    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
+    let horizontal = Vec3::new(4.0, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, 2.0, 0.0);
+    let origin = Vec3::new(0.0, 0.0, 0.0);
 
-            let ir = (255.99 * r) as i32;
-            let ig = (255.99 * g) as i32;
-            let ib = (255.99 * b) as i32;
+    for j in (0..ny).rev() {
+        for i in 0..nx {
+            let u = i as f64 / nx as f64;
+            let v = j as f64 / ny as f64;
+
+            let direction = lower_left_corner + (horizontal * u) + (vertical * v);
+            let r = Ray::new(origin, direction);
+
+            let col = color(&r);
+
+            let ir = (255.99 * col.x) as i32;
+            let ig = (255.99 * col.y) as i32;
+            let ib = (255.99 * col.z) as i32;
 
             println!("{} {} {}", ir, ig, ib);
         }
